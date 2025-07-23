b40e33accf3e1c27b49a7e00fd8d90e1a27671a5
clasindu <lasindura00@gmail.com>
2025-07-23 22:41:14 +0530

    First we start frontend part

---
+++ my-react-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChurnPredictionForm from './ChurnPredictionForm'; // Adjust path if needed
+
+function App() {
+  return (
+    <div className="App">
+      <ChurnPredictionForm />
+    </div>
+  );
+}
+
+export default App;
